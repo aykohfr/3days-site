f4fe708badd406421672099f1afc9d3f8fb16176
Aykoh <biscop70@gmail.com>
2026-09-08 23:13:00 +0200

    fix: use current lottie-react named export

--- src/Loader.tsx
+++ src/Loader.tsx
@@ -1,4 +1,4 @@
-import Lottie from 'lottie-react'
+import { Lottie } from 'lottie-react'
 import loaderAnimation from './ae/loader.json'
 
 export default function Loader({ leaving }: { leaving: boolean }) {
